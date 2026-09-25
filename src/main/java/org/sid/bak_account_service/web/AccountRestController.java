58160f0fa3403218f8f6db105e5c687984c4828a
hichamouaouche <hichamouaouche@gmail.com>
2026-09-25 23:53:29 +0000

    update

--- src/main/java/org/sid/bak_account_service/web/AccountRestController.java
+++ src/main/java/org/sid/bak_account_service/web/AccountRestController.java
@@ -2,10 +2,9 @@ package org.sid.bak_account_service.web;
 
 import org.sid.bak_account_service.entities.BankAccount;
 import org.sid.bak_account_service.repositories.BankAccountRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,27 @@ public class AccountRestController {
         return bankAccountRepository.findById(id)
                 .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
     }
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount){
+        return bankAccountRepository.save(bankAccount);
+    }
+
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id,  @RequestBody BankAccount bankAccount){
+        BankAccount account = bankAccountRepository.findById(id).orElseThrow();
+        if (bankAccount.getBalance() != null) account.setBalance(bankAccount.getBalance());
+        if (bankAccount.getCreatedAt() != null) account.setCreatedAt(new Date());
+        if (bankAccount.getType() != null) account.setType(bankAccount.getType());
+        if (bankAccount.getCurrency() != null) account.setCurrency(bankAccount.getCurrency());
+        return bankAccountRepository.save(account);
+    }
+
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount(@PathVariable String id){
+        bankAccountRepository.deleteById(id);
+    }
+
+
+
 
 }
