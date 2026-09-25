0a626bf7627d9ed4bb917279589f2dd14431848b
hichamouaouche <hichamouaouche@gmail.com>
2026-09-25 19:14:33 +0000

    fitst commit

---
+++ src/test/java/org/sid/bak_account_service/BakAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.bak_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
