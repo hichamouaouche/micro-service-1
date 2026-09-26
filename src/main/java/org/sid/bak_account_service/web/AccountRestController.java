bb81e6d849b43ae26247aa312ec3b1d5c21cd3b7
hichamouaouche <hichamouaouche@gmail.com>
2026-09-26 00:25:47 +0000

    update

--- src/main/java/org/sid/bak_account_service/web/AccountRestController.java
+++ src/main/java/org/sid/bak_account_service/web/AccountRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class AccountRestController {
@@ -29,6 +30,7 @@ public class AccountRestController {
     }
     @PostMapping("/bankAccounts")
     public BankAccount save(@RequestBody BankAccount bankAccount){
+        if(bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
         return bankAccountRepository.save(bankAccount);
     }
 
